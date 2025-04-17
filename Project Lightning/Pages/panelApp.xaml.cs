bee526c3ef35753d104505f5c01f3af0cb3b5a18
LightningFast <bydraxx4k129@gmail.com>
2025-04-17 15:41:38 +0200

    v0.0.2.9.1
    Había un error de compilación

--- Project Lightning/Pages/panelApp.xaml.cs
+++ Project Lightning/Pages/panelApp.xaml.cs
@@ -27,6 +27,25 @@ namespace Project_Lightning.Pages
             //CAMBIO EL NOMBRE DE LA ETIQUETA
             txtApp.Text = nomApp;
 
+            ponerJuegos(nomApp);
+
+        }
+
+        //CLASE JUEGO QUE CONTENDRA LA INFORMACIÓN DE CADA JUEGO
+        public class Juego
+        {
+            public string name { get; set; }
+            public bool launch_steam { get; set; }
+            public bool launch_exe { get; set; }
+            public string comentatios { get; set; }
+            public string errores { get; set; }
+            public string nombre_fix { get; set; }
+        }
+
+        private void ponerJuegos(string nomApp)
+        {
+            
+
 
         }
     }
