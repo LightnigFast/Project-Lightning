be7b7514814930896bf80d27694b8413b7494d2f
LightningFast <bydraxx4k129@gmail.com>
2025-04-19 16:42:26 +0200

    v0.0.6.0
    Todos los datos del json introducidos correctamente y listos
    
    Ahora estoy con el boton, ya funciona pero falta añadir funcionalidades

--- Project Lightning/Pages/panelJuego.xaml.cs
+++ Project Lightning/Pages/panelJuego.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,7 +14,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Compression;
 using Newtonsoft.Json;
+using SharpCompress.Archives;
+using SharpCompress.Common;
+using SharpCompress.Readers;
 using static Project_Lightning.Pages.panelApp;
 
 namespace Project_Lightning.Pages
@@ -27,6 +32,7 @@ namespace Project_Lightning.Pages
         private string nombreApp;
         private panelApp panelApp;
         private MainWindow ventanaPrincipal;
+        private KeyValuePair<string, panelApp.Juego> juego;
         public panelJuego(string nombreApp, KeyValuePair<string, panelApp.Juego> juego, panelApp panelApp, MainWindow ventanaPrincipal)
         {
             InitializeComponent();
@@ -34,6 +40,7 @@ namespace Project_Lightning.Pages
             this.nombreApp = nombreApp;
             this.panelApp = panelApp;
             this.ventanaPrincipal = ventanaPrincipal;
+            this.juego = juego;
 
             crearBackground(juego.Key);
             crearLogo(juego.Key);
@@ -125,14 +132,124 @@ namespace Project_Lightning.Pages
             //SACAR EL APARTADO DE ERROES
             txtErrors.Text = juego.Value.errores;
 
+            //SACAR EL APARTADO DE COMENTARIOS
+            txtComentarios.Text = juego.Value.comentarios;
+
         }
 
-         
+
+        //CLICK PARA EL BOTON DE FIXEAR
+        private void fixButtonClick(object sender, RoutedEventArgs e)
+        {
+            descargarJuego(juego);
+        }
 
         //CLICK DEL BOTON DE VOLVER
         private void volverClick(object sender, RoutedEventArgs e)
         {
             ventanaPrincipal.framePrincipal.Navigate(new panelApp(nombreApp, ventanaPrincipal));
+        }
+
+
+
+
+        //METODO PARA DESCARGAR LOS ARCHIVOS FIX DE UN JUEGO INTEPENDIENTE
+        private async void descargarJuego(KeyValuePair<string, Juego> keyValuePair)
+        {
+            string appId = keyValuePair.Key;
+            string user = "LightnigFast";
+            string repo = "gamesFixes";
+            string token = "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";
+
+            string apiUrl = $"https://api.github.com/repos/{user}/{repo}/contents/{appId}";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("request");
+            client.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            try
+            {
+                string json = await client.GetStringAsync(apiUrl);
+                var archivos = JsonConvert.DeserializeObject<List<ArchivoGitHub>>(json);
+
+                string escritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string carpetaDestino = System.IO.Path.Combine(escritorio, appId);
+                Directory.CreateDirectory(carpetaDestino);
+
+                string rutaZipExtraer = null;
+
+                // DESCARGAR TODO
+                foreach (var archivo in archivos)
+                {
+                    if (archivo.type == "file")
+                    {
+                        string rutaDestino = System.IO.Path.Combine(carpetaDestino, archivo.name);
+                        byte[] datos = await client.GetByteArrayAsync(archivo.download_url);
+                        File.WriteAllBytes(rutaDestino, datos);
+
+                        // GUARDAR RUTA AL PRIMER ZIP ENCONTRADO PARA EXTRAERLO DESPUÉS
+                        if (archivo.name.EndsWith(".zip") && rutaZipExtraer == null)
+                            rutaZipExtraer = rutaDestino;
+                    }
+                }
+
+                // EXTRAER ZIP SI EXISTE
+                if (rutaZipExtraer != null)
+                {
+                    string carpetaTemporal = System.IO.Path.Combine(carpetaDestino, "temp_extraccion");
+
+                    // Usamos SharpCompress para manejar archivos zip
+                    using (var archive = ArchiveFactory.Open(rutaZipExtraer))
+                    {
+                        foreach (var entry in archive.Entries)
+                        {
+                            if (!entry.IsDirectory)
+                            {
+                                string filePath = System.IO.Path.Combine(carpetaTemporal, entry.Key);
+                                Directory.CreateDirectory(System.IO.Path.GetDirectoryName(filePath)); // Crear directorios si no existen
+
+                                // Extraer el archivo
+                                using (var fileStream = File.Create(filePath))
+                                {
+                                    entry.WriteTo(fileStream);  // Extrae el contenido del archivo
+                                }
+                            }
+                        }
+                    }
+
+                    // MOVER CONTENIDO EXTRAÍDO A CARPETA FINAL
+                    foreach (var archivoExtraido in Directory.GetFiles(carpetaTemporal, "*", SearchOption.AllDirectories))
+                    {
+                        string nombreRelativo = archivoExtraido.Substring(carpetaTemporal.Length + 1);
+                        string rutaFinal = System.IO.Path.Combine(carpetaDestino, nombreRelativo);
+
+                        Directory.CreateDirectory(System.IO.Path.GetDirectoryName(rutaFinal));
+                        File.Move(archivoExtraido, rutaFinal);
+                    }
+
+                    // Eliminar archivos temporales
+                    Directory.Delete(carpetaTemporal, true);
+                    File.Delete(rutaZipExtraer);
+                }
+
+                MessageBox.Show("Descarga y extracción completadas: " + carpetaDestino);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+
+
+        //CLASE AUXILIAR PARA DESERIALIZAR LA RESPUESTA DE GITHUB API
+        public class ArchivoGitHub
+        {
+            public string name { get; set; }
+            public string path { get; set; }
+            public string type { get; set; }
+            public string download_url { get; set; }
         }
     }
 }
