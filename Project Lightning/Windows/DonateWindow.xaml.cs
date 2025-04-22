ce6009641efa908fddc684719430027b5e78c3c1
LightningFast <bydraxx4k129@gmail.com>
2025-04-22 13:22:44 +0200

    v0.0.7.7
    Ventana de donación completada

--- Project Lightning/Windows/DonateWindow.xaml.cs
+++ Project Lightning/Windows/DonateWindow.xaml.cs
@@ -24,11 +24,12 @@ namespace Project_Lightning.Windows
         {
             InitializeComponent(); 
             cargarGif();
+            cargarGifDiscord();
         }
 
         private void cargarGif()
         {
-            var gifPath = "/res/media/logos/originals/donate.gif";  //RUTA RELATIVA
+            var gifPath = "/res/media/logos/originals/purple-heart.gif";  //RUTA RELATIVA
 
             var image = new BitmapImage();
             image.BeginInit();
@@ -36,12 +37,41 @@ namespace Project_Lightning.Windows
             image.EndInit();
 
             ImageBehavior.SetAnimatedSource(gifImage, image);
+
         }
 
         //EVENTO PARA EL BOTON DE SALIR
         private void botonSalirClick(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        //EVENTO PARA EL BOTON DE DISCORD
+        private void botonDiscord_Click(object sender, RoutedEventArgs e)
+        {
+            //URL DE MI PERFIL DE DISCORD
+            string url = "https://discord.com/users/507878272072155148";
+
+            //ABRIR URL EN EL NAVEGADOR PREDETERMINADO
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+
+        //METODO PARA CARGAR EL GIF DEL BOTON DE FISCORD
+        private void cargarGifDiscord()
+        {
+            var gifPath = "/res/media/logos/originals/discord_logo.gif";  //RUTA RELATIVA
+
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(gifPath, UriKind.Relative);
+            image.EndInit();
+
+            ImageBehavior.SetAnimatedSource(gifImageButton, image);
+
         }
 
     }
