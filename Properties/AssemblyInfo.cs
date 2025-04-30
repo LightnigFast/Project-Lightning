64dd1ac2265b92e412728bf474451b3b464e3ebc
LightningFast <bydraxx4k129@gmail.com>
2025-04-30 19:09:45 +0200

    v0.1.0.0
    Version estable, vamos a ello

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Número de compilación
 //      Revisión
 //
-[assembly: AssemblyVersion("0.0.9.9")]
-[assembly: AssemblyFileVersion("0.0.9.9")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
