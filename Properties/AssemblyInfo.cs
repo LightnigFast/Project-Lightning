eb5e897411536d13cb8d9aa19c2b15658112b25b
LightningFast <bydraxx4k129@gmail.com>
2025-07-22 12:11:49 +0200

    v0.2.0.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      Número de compilación
 //      Revisión
 //
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.0.3")]
+[assembly: AssemblyFileVersion("0.2.0.3")]
